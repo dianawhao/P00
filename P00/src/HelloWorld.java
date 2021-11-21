7678f60da108dc4749d1afffec63881678ec38a7
diana_wang <diana_wang@SFT-OIS31522.rp.edu.sg>
2021-11-21 10:41:58 +0800

    test github

--- P00/src/HelloWorld.java
+++ P00/src/HelloWorld.java
@@ -1,11 +1,17 @@
+import java.util.regex.*;  
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("My First Java Program, to demo git");
-
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the licensePlate: ");
+        String myString = input.next().toUpperCase();
+	
+		boolean b = Pattern.matches("[A-Z]{3}[0-9]{4}[A-Z]", myString);
+		System.out.println("a valid license plate? " + b);
 	}
-
+	
 }
